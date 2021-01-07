3be44cd00c1b637035fd3df8c26cc91e331c0688
imns1ght <jeffersonbrunoit@gmail.com>
2021-01-07 12:02:41 -0300

    Fix Last Releases category and updates dependencies
    - The date in the API request for the latest releases was incorrect
    - Fix security issues

--- src/services/api.ts
+++ src/services/api.ts
@@ -21,12 +21,10 @@ import { CONSTANTS } from "./constants";
 
 const getCurrentDate = () => {
   const currentDate = new Date();
+  const currentMonth = (currentDate.getMonth() + 1).toString().padStart(2, "0");
+  const currentDay = currentDate.getDate().toString().padStart(2, "0");
   const currentDateApiPattern =
-    currentDate.getFullYear() +
-    "-" +
-    currentDate.getMonth() +
-    "-" +
-    currentDate.getDate();
+    currentDate.getFullYear() + "-" + currentMonth + "-" + currentDay;
   return currentDateApiPattern;
 };
 
