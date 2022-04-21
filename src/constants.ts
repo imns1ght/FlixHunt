91e009801028e411de6b3b0a91ade63c37e1eb4d
imns1ght <jeffersonbrunoit@gmail.com>
2022-04-21 12:55:43 -0300

    add warn

--- src/constants.ts
+++ src/constants.ts
@@ -1,7 +1,7 @@
 const CONSTANTS = {
   api_base_url: 'https://api.themoviedb.org/3',
   api_image_url: 'https://image.tmdb.org/t/p',
-  api_key: '3977028d418452ab5ad05c8bb732a7e0',
+  api_key: '3977028d418452ab5ad05c8bb732a7e0', // TODO: move to a secure place
 }
 
 export default CONSTANTS
